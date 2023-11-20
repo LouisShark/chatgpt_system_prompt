b02dd1c0f0f6c72b2eb94d2b291562fefc255afb
LouisShark <mshark.louis@gmail.com>
2023-11-20 20:33:09 +0800

    fix: replace colon or other symbol（some systems don't support） in filename

--- generate_toc_for_readme.sh
+++ generate_toc_for_readme.sh
@@ -11,9 +11,12 @@ generate_toc() {
             echo "  $(generate_toc "$file")"
         elif [ -f "$file" ] && [[ $file == *.md ]]; then
             local title=$(basename "$file" .md)
+            # Replace Windows incompatible characters with '_'
+            local safe_title=$(echo "$title" | sed 's/[<>:："\/\\|?*]/_/g')
+            # shellcheck disable=SC2001
             local link=$(echo "$file" | sed "s|^$base_dir/||")
             local encoded_link=$(python -c "import urllib.parse; print(urllib.parse.quote('''$link'''))")
-            echo "  - [$title]($encoded_link)"
+            echo "  - [$safe_title]($encoded_link)"
         fi
     done
 }
